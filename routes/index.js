13b27548bf1487709823b3b86dc26f2bb2058454
ccou <celia.couiller@outlook.fr>
2018-07-21 18:02:26 +0200

    [ToutDouxList] arrow function on gets

--- routes/index.js
+++ routes/index.js
@@ -2,89 +2,89 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Express' });
 });
 
 const myObject = require("../data/generated.json");
 console.log("Data loaded");
 console.log(myObject);
 
-sendError = (res,erroMessage) => {
-  res.status(500);
-  res.send(erroMessage);
+sendError = (res, erroMessage) => {
+    res.status(500);
+    res.send(erroMessage);
 }
 
-router.get('/categories', function(req, res, next) {
-  if (myObject.categories){
-    res.send(myObject.categories);
-  } else {
-    sendError( res, `Can't find any categories`);
-  }  
-});
-
-router.get('/categories/:categorie_id', function(req, res, next) {  
-  if (myObject.categories){
-    const categoryId = req.params.categorie_id;
-    const category = myObject.categories.find((c)=>c.id==categoryId);
-    if (category){
-        res.send(category);
+router.get('/categories', (req, res, next) => {
+    if (myObject.categories) {
+        res.send(myObject.categories);
     } else {
-        sendError( res,  `Can't find a categorie with id ${categoryId}`);   
-
+        sendError(res, `Can't find any categories`);
     }
-  } else {
-    sendError( res, `Can't find any categories`);
-  }
 });
 
-router.get('/categories/:categorie_id/items', function(req, res, next) {
-    if (myObject.categories){
+router.get('/categories/:categorie_id', (req, res, next) => {
+    if (myObject.categories) {
         const categoryId = req.params.categorie_id;
-        const category = myObject.categories.find((c)=>c.id==categoryId);
-        if (category){
+        const category = myObject.categories.find((c) => c.id == categoryId);
+        if (category) {
+            res.send(category);
+        } else {
+            sendError(res, `Can't find a categorie with id ${categoryId}`);
+
+        }
+    } else {
+        sendError(res, `Can't find any categories`);
+    }
+});
+
+router.get('/categories/:categorie_id/items', (req, res, next) => {
+    if (myObject.categories) {
+        const categoryId = req.params.categorie_id;
+        const category = myObject.categories.find((c) => c.id == categoryId);
+        if (category) {
             const items = category.items;
-            if(items){
+            if (items) {
                 res.send(items);
-            }else {
-                sendError( res, `Can't find any items in this category`);
+            } else {
+                sendError(res, `Can't find any items in this category`);
             }
         } else {
-            sendError( res,  `Can't find a categorie with id ${categoryId}`);   
-    
+            sendError(res, `Can't find a categorie with id ${categoryId}`);
+
         }
-      } else {
-        sendError( res, `Can't find any categories`);
-      }
+    } else {
+        sendError(res, `Can't find any categories`);
+    }
 });
 
-router.get('/categories/:categorie_id/items/:item_id', function(req, res, next) {
-    if (myObject.categories){
+router.get('/categories/:categorie_id/items/:item_id', (req, res, next) => {
+    if (myObject.categories) {
         const categoryId = req.params.categorie_id;
-        const category = myObject.categories.find((c)=>c.id==categoryId);
-        if (category){
+        const category = myObject.categories.find((c) => c.id == categoryId);
+        if (category) {
             const items = category.items;
-            if(items){
+            if (items) {
                 const itemId = req.params.item_id;
-                const item = category.items.find((c)=>c.id==itemId);
-                if(item){
+                const item = category.items.find((c) => c.id == itemId);
+                if (item) {
                     res.send(item);
-                }else {
-                    sendError( res,  `Can't find an item with id ${itemId}`)
+                } else {
+                    sendError(res, `Can't find an item with id ${itemId}`)
                 }
-            }else {
-                sendError( res, `Can't find any items in this category`);
+            } else {
+                sendError(res, `Can't find any items in this category`);
             }
         } else {
-            sendError( res,  `Can't find a categorie with id ${categoryId}`);   
-    
+            sendError(res, `Can't find a categorie with id ${categoryId}`);
+
         }
-      } else {
-        sendError( res, `Can't find any categories`);
-      }
+    } else {
+        sendError(res, `Can't find any categories`);
+    }
 });
 
-// router.put('/categories/:categoryId/items/:name', function(req, res, next) {
+// router.put('/categories/:categoryId/items/:name', (req, res, next) => {
 //   const categoryId = req.params.categoryId;
 //   const itemName = req.params.name;
 //   myObject.categories[categoryId].items.add({id:getItemID(categoryId),name:itemName});
